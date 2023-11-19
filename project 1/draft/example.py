84e4a3065b18bc4822ba5ffec6fa1d90db55e1ed
bordolot <otori29@wp.pl>
2023-11-19 23:39:23 +0100

    new updates

--- project 1/draft/example.py
+++ project 1/draft/example.py
@@ -1,7 +1,16 @@
-x = "helloworld"
-byte_array = "helloworld".encode('utf-8')
+# x = "helloworld"
+# byte_array = "helloworld".encode('utf-8')
 
-for bite in byte_array:
-    print(bite)
+# for bite in byte_array:
+#     print(bite)
 
 # print(byte_array)
+
+# with open("../../contracts/Calculator.sol", 'r') as file:
+#     simple_storage_file = file.read()
+
+# with open("../../contracts/Calculator.sol", 'r') as file:
+#     simple_storage_file = file.read()
+
+import os.path
+os.path.isfile("./example.py") 
